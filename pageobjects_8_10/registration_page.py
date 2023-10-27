d4477057bc8531f44677e624a03ccc93f31f08b4
Polina Vishnyakova <vishnyakovapv@gmail.com>
2023-10-27 18:56:32 +0400

    init commit

---
+++ pageobjects_8_10/registration_page.py
@@ -0,0 +1,82 @@
+from selene import browser, be, by, command, have
+
+
+# class registration_page():
+#     def open(self, address):
+#         browser.open(address)
+#     def
+#
+#
+# def open(param):
+#     return None
+class RegistrationPage:
+    def __init__(self):
+        pass
+
+    def open(self):
+        browser.open('/automation-practice-form')
+        return self
+
+    def fill_first_name(self, name):
+        browser.element('#firstName').should(be.blank).type(name)
+        return self
+
+    def fill_last_name(self, last_name):
+        browser.element('#lastName').type(last_name)
+        return self
+
+    def email(self, email):
+        browser.element('#userEmail').type(email)
+        return self
+
+    def gender(self):
+        browser.element('[for=gender-radio-2]').click()
+        return self
+
+    def phone(self, number):
+        browser.element('#userNumber').should(be.blank).type(number)
+        return self
+
+    def birthdate(self, month, year):
+        browser.element('#dateOfBirthInput').click()
+        browser.element('.react-datepicker__month-select').click().element(by.text(month)).click()
+        browser.element('.react-datepicker__year-select').click().element(by.text(year)).click()
+        browser.element('.react-datepicker__day.react-datepicker__day--025').click()
+        return self
+
+    def fill_subject(self, subject_name):
+        browser.element('#subjectsInput').type(subject_name).press_enter()
+        return self
+
+    def fill_hobbies(self, type):
+        browser.element('#hobbiesWrapper').perform(command.js.scroll_into_view).element(by.text(type)).click()
+        return self
+
+    def fill_address(self, value):
+        browser.element('#currentAddress').type(value)
+        return self
+
+    def state_and_city(self, state, city):
+        browser.element('#state').click().element(by.text(state)).click()
+        browser.element('#city').click().element(by.text(city)).click()
+        return self
+
+    def submit(self):
+        browser.element('#submit').press_enter()
+        return self
+
+    def should_registered_user_with(self, name, email, gender, phone, birthdate, subject, hobby, file, address,
+                                    state_city):
+        browser.element('.table').all('tr td:nth-child(2)').should(have.texts(
+            name,
+            email,
+            gender,
+            phone,
+            birthdate,
+            subject,
+            hobby,
+            file,
+            address,
+            state_city
+        ))
+        return self
--- tests/test_fill_in_and_send_form.py
+++ tests/test_fill_in_and_send_form.py
@@ -1,51 +1,42 @@
 import os
-import resource
-from selene import browser, be, have, by, command
+from selene import browser
+from pageobjects_8_10.registration_page import RegistrationPage
 
 
 def test_form():
-    browser.open('/automation-practice-form')
+    registration_page = RegistrationPage()
+    registration_page.open()
     # Name and Email
-    browser.element('#firstName').should(be.blank).type('Test_name')
-    browser.element('#lastName').should(be.blank).type('Test_Lastname')
-    browser.element('#userEmail').should(be.blank).type('Test@test.com')
-    # Choose gender +
-    browser.element('[for=gender-radio-2]').click()
+    registration_page.fill_first_name('Test_name')
+    registration_page.fill_last_name('Test_Lastname')
+    registration_page.email('Test@test.com')  # browser.element('#userEmail').should(be.blank).type('Test@test.com')
+    # Choose gender
+    registration_page.gender()  # browser.element('[for=gender-radio-2]').click()
     # Write phone
-    browser.element('#userNumber').should(be.blank).type('1234567890')
+    registration_page.phone('1234567890')  # browser.element('#userNumber').should(be.blank).type('1234567890')
     # Calendar
-    browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker__month-select').click().element(by.text('December')).click()
-    browser.element('.react-datepicker__year-select').click().element(by.text('2020')).click()
-    browser.element('.react-datepicker__day.react-datepicker__day--025').click()
-
+    registration_page.birthdate('December', '2020')
     # Subject
-    browser.element('#subjectsInput').type('Maths').press_enter()
+    registration_page.fill_subject('Maths')
     # Choose hobby
-    # browser.element('[for=hobbies-checkbox-1]').click()
-    browser.element('#hobbiesWrapper').perform(command.js.scroll_into_view).element(by.text('Sports')).click()
+    registration_page.fill_hobbies('Sports')
     # download file
     browser.element('#uploadPicture').send_keys(os.path.abspath('resourses/123.png'))
     # Write address
-    browser.element('#currentAddress').type('Tbilisi')
+    registration_page.fill_address('Tbilisi')
     # Choose country and city
-    browser.element('#state').click().element(by.text('NCR')).click()
-    browser.element('#city').click().element(by.text('Delhi')).click()
-
+    registration_page.state_and_city('NCR', 'Delhi')
     # Press button
-    browser.element('#submit').press_enter()
+    registration_page.submit()
+    registration_page.should_registered_user_with('Test_name Test_Lastname',
+                                                  'Test@test.com',
+                                                  'Female',
+                                                  '1234567890',
+                                                  '25 December,2020',
+                                                  'Maths',
+                                                  'Sports',
+                                                  '123.png',
+                                                  'Tbilisi',
+                                                  'NCR Delhi')
 
-    browser.element('.table').all('tr td:nth-child(2)').should(have.texts(
-        'Test_name Test_Lastname',
-        'Test@test.com',
-        'Female',
-        '1234567890',
-        '25 December,2020',
-        'Maths',
-        'Sports',
-        '123.png',
-        'Tbilisi',
-        'NCR Delhi'
-    ))
-
-    browser.element('#closeLargeModal').press_escape()  #кнопка перекрыта рекламой
+    browser.element('#closeLargeModal').press_escape()  # кнопка перекрыта рекламой
