3d7d27dd2e60966c9c81da2b3fd58f5986e296ba
Polina Vishnyakova <vishnyakovapv@gmail.com>
2023-11-01 20:22:09 +0400

    добавлена работа с дженкинс и отчеты

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,21 +11,21 @@ def browser_settings():
     browser.config.window_height = 900
     browser.config.window_width = 1440
     browser.config.base_url = 'https://demoqa.com'
-    # options = Options()
-    # selenoid_capabilities = {
-    #     "browserName": "chrome",
-    #     "browserVersion": "100.0",
-    #     "selenoid:options": {
-    #         "enableVNC": True,
-    #         "enableVideo": True
-    #     }
-    # }
-    # options.capabilities.update(selenoid_capabilities)
-    # driver = webdriver.Remote(
-    #     command_executor="https://user1%1234@selenoid:autotests.cloud/wd/hub",
-    #     options=options)
-    #
-    # browser.config.driver = driver
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor="https://user1%1234@selenoid:autotests.cloud/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
 
     yield browser
 
