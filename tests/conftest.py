d35eee1fd3466fc1d53b3a37df22771902a51d12
Polina Vishnyakova <vishnyakovapv@gmail.com>
2023-11-01 19:23:42 +0400

    добавлена работа с дженкинс и отчеты

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,37 @@
 import pytest
+from selenium import webdriver
 from selene import browser
+from selenium.webdriver.chrome.options import Options
+
+from utils import attach
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_settings():
-    browser.config.window_height = 900
-    browser.config.window_width = 1440
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor="https://user1%1234@selenoid:autotests.cloud/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
     browser.config.base_url = 'https://demoqa.com'
 
     yield
+    attach.add_screenshot(browser)
+
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
 
     browser.quit()
+
--- tests/test_fill_in_and_send_form.py
+++ tests/test_fill_in_and_send_form.py
@@ -1,42 +1,48 @@
+import allure
 from selene import browser, have, command
 from pageobjects_8_10.registration_page import RegistrationPage
 
 
+@allure.title("Fill form")
 def test_form():
     registration_page = RegistrationPage()
-    registration_page.open()
-    registration_page.remove_ads()
-    # Name and Email
-    registration_page.fill_first_name('Test_name')
-    registration_page.fill_last_name('Test_Lastname')
-    registration_page.email('Test@test.com')  # browser.element('#userEmail').should(be.blank).type('Test@test.com')
-    # Choose gender
-    registration_page.gender()  # browser.element('[for=gender-radio-2]').click()
-    # Write phone
-    registration_page.phone('1234567890')  # browser.element('#userNumber').should(be.blank).type('1234567890')
-    # Calendar
-    registration_page.birthdate('25', 'December', '2020')
-    # Subject
-    registration_page.fill_subject('Maths')
-    # Choose hobby
-    registration_page.fill_hobbies('Sports')
-    # download file
-    registration_page.download_file('123.png')
-    # Write address
-    registration_page.fill_address('Tbilisi')
-    # Choose country and city
-    registration_page.state_and_city('NCR', 'Delhi')
-    # Press button
-    registration_page.submit()
-    registration_page.should_registered_user_with('Test_name Test_Lastname',
-                                                  'Test@test.com',
-                                                  'Female',
-                                                  '1234567890',
-                                                  '25 December,2020',
-                                                  'Maths',
-                                                  'Sports',
-                                                  '123.png',
-                                                  'Tbilisi',
-                                                  'NCR Delhi')
+    with allure.step("Open registrations form"):
+        registration_page.open()
+    with allure.step("Fill registrations form"):
+        registration_page.remove_ads()
+        # Name and Email
+        registration_page.fill_first_name('Test_name')
+        registration_page.fill_last_name('Test_Lastname')
+        registration_page.email('Test@test.com')  # browser.element('#userEmail').should(be.blank).type('Test@test.com')
+        # Choose gender
+        registration_page.gender()  # browser.element('[for=gender-radio-2]').click()
+        # Write phone
+        registration_page.phone('1234567890')  # browser.element('#userNumber').should(be.blank).type('1234567890')
+        # Calendar
+        registration_page.birthdate('25', 'December', '2020')
+        # Subject
+        registration_page.fill_subject('Maths')
+        # Choose hobby
+        registration_page.fill_hobbies('Sports')
+        # download file
+        registration_page.download_file('123.png')
+        # Write address
+        registration_page.fill_address('Tbilisi')
+        # Choose country and city
+        registration_page.state_and_city('NCR', 'Delhi')
+        # Press button
+        registration_page.submit()
 
-    browser.element('#closeLargeModal').press_escape()  # кнопка перекрыта рекламой
+    with allure.step("Check registrations form"):
+        registration_page.should_registered_user_with('Test_name Test_Lastname',
+                                                      'Test@test.com',
+                                                      'Female',
+                                                      '1234567890',
+                                                      '25 December,2020',
+                                                      'Maths',
+                                                      'Sports',
+                                                      '123.png',
+                                                      'Tbilisi',
+                                                      'NCR Delhi')
+    with allure.step("Close  form"):
+        browser.element('#closeLargeModal').press_escape()  # кнопка перекрыта рекламой
