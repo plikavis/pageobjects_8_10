28619fd2a48514b72f044b54332cc345b4aa7500
Polina Vishnyakova <vishnyakovapv@gmail.com>
2023-11-02 13:19:05 +0400

    add conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,12 @@
 import pytest
+from selene import browser
 from selenium import webdriver
-from selene import browser, Browser, Config
 from selenium.webdriver.chrome.options import Options
-
 from utils import attach
 
 
 @pytest.fixture(scope='function', autouse=True)
-def browser_settings():
-    # browser.config.window_height = 900
-    # browser.config.window_width = 1440
-    # browser.config.base_url = 'https://demoqa.com'
+def browser_settings(request):
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -21,11 +17,12 @@ def browser_settings():
         }
     }
     options.capabilities.update(selenoid_capabilities)
-    driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options)
-
-    browser = Browser(Config(driver))
+    driver = webdriver.Remote(command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub", options=options)
+    browser.config.base_url = "https://demoqa.com"
+    browser.config.driver = driver
+    browser.config.timeout = 2.0
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
 
     yield browser
 
@@ -35,5 +32,3 @@ def browser_settings():
     attach.add_video(browser)
 
     browser.quit()
-
-
